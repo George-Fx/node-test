320d92a8806492c45e15e7dfdb79d6701592b103
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 11:39:35 +0400

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("This is test node!");
 });
 
 const PORT = process.env.PORT || 8000;
